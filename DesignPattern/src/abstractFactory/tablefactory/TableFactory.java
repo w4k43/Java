0ef2602a2984a3f3763bee7cdd66df08b905d95d
waka <wakame0912@gmail.com>
2019-12-29 15:48:44 +0900

    add tablefactory

---
+++ DesignPattern/src/abstractFactory/tablefactory/TableFactory.java
@@ -0,0 +1,25 @@
+package abstractFactory.tablefactory;
+
+import abstractFactory.factory.Factory;
+import abstractFactory.factory.Link;
+import abstractFactory.factory.Page;
+import abstractFactory.factory.Tray;
+
+public class TableFactory extends Factory {
+
+	@Override
+	public Link createLink(String caption, String url) {
+		return new TableLink(caption, url);
+	}
+
+	@Override
+	public Tray createTray(String caption) {
+		return new TableTray(caption);
+	}
+
+	@Override
+	public Page createPage(String title, String author) {
+		return new TablePage(title, author);
+	}
+
+}
